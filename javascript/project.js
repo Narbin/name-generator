eb650b01c6b62f226494e278ce3c0328f04e5d8e
Arek <Solaris1995@gmail.com>
2016-07-11 22:59:12 +0200

    Fixed random first letter, and name have to have min. 4 letters

--- javascript/project.js
+++ javascript/project.js
@@ -8,15 +8,15 @@ var generateName = function(){
 	var name = "",
 		consonant = "bcdfghklmnpqrstvwxz",
 		vowel = "aeijouy";
-    	nameLength = Math.floor(Math.random()*9)+1,
+    	nameLength = Math.floor(Math.random()*9)+4,
     	whatLetterFirst = Math.floor(Math.random()*2),
     	firstLetterIs = null;
 
     	if(whatLetterFirst == 1){
-    		name += consonant.charAt(Math.floor(Math.random() * nameLength));
+    		name += consonant.charAt(Math.floor(Math.random() * consonant.length));
     		firstLetterIs = "consonant";
     	}else if(whatLetterFirst == 0){
-    		name += vowel.charAt(Math.floor(Math.random() * nameLength));
+    		name += vowel.charAt(Math.floor(Math.random() * vowel.length));
     		firstLetterIs = "vowel";
     	}
 
