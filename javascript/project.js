aea43a5ca1a3450fab88b37a7263e3a17c05e385
Arek <Solaris1995@gmail.com>
2016-07-11 22:54:28 +0200

    Revert "Random first letter, showing name in name-space"
    This reverts commit d6f7fc2d6c7cffae308b7d49e454b54336d9ae5d.

--- javascript/project.js
+++ javascript/project.js
@@ -1,26 +1,3 @@
 $(document).ready(function(){
-	$(".button-name").click(function(){
-		generateName();
-	});
+	
 });
-
-var generateName = function(){
-	var name = "",
-		consonant = "bcdfghklmnpqrstvwxz",
-		vowel = "aeijouy";
-    	nameLength = Math.floor(Math.random()*9)+1,
-    	whatLetterFirst = Math.floor(Math.random()*2),
-    	firstLetterIs = null;
-
-    	if(whatLetterFirst == 1){
-    		name += consonant.charAt(Math.floor(Math.random() * nameLength));
-    		firstLetterIs = "consonant";
-    	}else if(whatLetterFirst == 0){
-    		name += vowel.charAt(Math.floor(Math.random() * nameLength));
-    		firstLetterIs = "vowel";
-    	}
-
-
-    
-    $(".name").html(name); 
-}
