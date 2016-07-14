d375542f4483ae9ab4790039af3a42710448e2c7
Arek <Solaris1995@gmail.com>
2016-07-14 14:38:40 +0200

    Correcting code and refactoring

--- javascript/project.js
+++ javascript/project.js
@@ -1,54 +1,53 @@
 $(document).ready(function(){
 
 	function createNewName(){
-		var newName = new name('bcdfghjklmnpqrstvwxz','aeiouy');
+		var newName = new Name('bcdfghjklmnpqrstvwxz', 'aeiouy', '.name', randomNumber(5,4));
 		newName.setFirstLetter();
 		newName.generateName();
 		newName.firstLetterUpper();
 		console.log(newName);
 	}
 
-	function name(_consonant,_vowel){
+	function Name(_consonant, _vowel, _localization, _nameLength){
 		this.consonant = _consonant;
 		this.vowel = _vowel;
 		this.consonantLength = this.consonant.length;
 		this.vowelLength = this.vowel.length;
-		this.nameLength = randomNumber(5,4);
+		this.nameLength = _nameLength;
 		this.name = '';
-		this.where = $('.name');
+		this.where = $(_localization);
 	}
 
-	name.prototype.setFirstLetter = function(){
-		this.nextLetter = null;
+	Name.prototype.setFirstLetter = function(){
 		this.whatLetterFirst = randomNumber(2,0);
 		if(this.whatLetterFirst == 1){
-			this.name += this.consonant.charAt(randomNumber(this.consonantLength,0));
-			this.nextLetter = "vowel";
+			this.name += this.consonant.charAt(randomNumber(this.consonantLength, 0));
+			this.nextLetter = 'vowel';
 		}else if(this.whatLetterFirst === 0){
-			this.name += this.vowel.charAt(randomNumber(this.vowelLength,0));
-			this.nextLetter = "consonant";
+			this.name += this.vowel.charAt(randomNumber(this.vowelLength, 0));
+			this.nextLetter = 'consonant';
 		}
 		return this.nextLetter;
 	};
 
-	name.prototype.generateName = function(){
+	Name.prototype.generateName = function(){
 		for( var i = 0; i < this.nameLength; i += 1 ){
-			if(this.nextLetter === "consonant"){
-				this.name += this.consonant.charAt(randomNumber(this.consonant.length,0));
-				this.nextLetter = "vowel";
+			if(this.nextLetter === 'consonant'){
+				this.name += this.consonant.charAt(randomNumber(this.consonant.length, 0));
+				this.nextLetter = 'vowel';
 			}else{
-				this.name += this.vowel.charAt(randomNumber(this.vowel.length,0));
-				this.nextLetter = "consonant";
+				this.name += this.vowel.charAt(randomNumber(this.vowel.length, 0));
+				this.nextLetter = 'consonant';
 			}
 		}
 	};
 
-	name.prototype.firstLetterUpper = function(){
+	Name.prototype.firstLetterUpper = function(){
 		this.where.html(this.name.charAt(0).toUpperCase() + this.name.slice(1)); 
 	};
 
-	function randomNumber(to,from){
-		return Math.floor(Math.random()*to)+from;
+	function randomNumber(to, from){
+		return Math.floor(Math.random() * to) + from;
 	}
 
 	$(".button-name").click(createNewName);
