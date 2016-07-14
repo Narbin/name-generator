f1a72b7150e41d9698b6a8564a3ff8514d0522ad
Arek <Solaris1995@gmail.com>
2016-07-14 14:23:05 +0200

    Correcting code and refactoring

--- javascript/project.js
+++ javascript/project.js
@@ -1,42 +1,35 @@
 $(document).ready(function(){
 
-	var application = {
-		name : "",
-		consonant : "bcdfghjklmnpqrstvwxz",
-		vowel : "aeiouy",
-		nameLength : null,
-		whatLetterFirst : null,
-		nextLetter : null
-	}
-
 	function createNewName(){
 		var newName = new name('bcdfghjklmnpqrstvwxz','aeiouy');
 		newName.setFirstLetter();
 		newName.generateName();
-		newName.firstLetterUpper('.name',newName.name);
+		newName.firstLetterUpper();
 		console.log(newName);
 	}
 
 	function name(_consonant,_vowel){
 		this.consonant = _consonant;
 		this.vowel = _vowel;
+		this.consonantLength = this.consonant.length;
+		this.vowelLength = this.vowel.length;
 		this.nameLength = randomNumber(5,4);
-		this.name = ''
+		this.name = '';
+		this.where = $('.name');
 	}
 
 	name.prototype.setFirstLetter = function(){
-		this.nextLetter = null,
+		this.nextLetter = null;
 		this.whatLetterFirst = randomNumber(2,0);
 		if(this.whatLetterFirst == 1){
-			this.name += this.consonant.charAt(randomNumber(this.consonant.length,0));
+			this.name += this.consonant.charAt(randomNumber(this.consonantLength,0));
 			this.nextLetter = "vowel";
-		}else if(this.whatLetterFirst == 0){
-			this.name += this.vowel.charAt(randomNumber(this.vowel.length,0));
+		}else if(this.whatLetterFirst === 0){
+			this.name += this.vowel.charAt(randomNumber(this.vowelLength,0));
 			this.nextLetter = "consonant";
 		}
-		return this.nextLetter
-
-	}
+		return this.nextLetter;
+	};
 
 	name.prototype.generateName = function(){
 		for( var i = 0; i < this.nameLength; i += 1 ){
@@ -48,18 +41,17 @@ $(document).ready(function(){
 				this.nextLetter = "consonant";
 			}
 		}
-	}
+	};
 
-	name.prototype.firstLetterUpper = function(where,string){
-		$(where).html(string.charAt(0).toUpperCase() + string.slice(1)); 
-	}
+	name.prototype.firstLetterUpper = function(){
+		this.where.html(this.name.charAt(0).toUpperCase() + this.name.slice(1)); 
+	};
 
 	function randomNumber(to,from){
 		return Math.floor(Math.random()*to)+from;
 	}
 
-	$(".button-name").click(function(){
-		createNewName();
-	});
+	$(".button-name").click(createNewName);
+
 });
 
