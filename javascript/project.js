986baac1d0499ebc284b87bdeae3e7f53cec0512
Arek <Solaris1995@gmail.com>
2016-07-14 18:00:07 +0200

    Refactoring code, added gulp support

--- javascript/project.js
+++ javascript/project.js
@@ -1,18 +1,10 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+/* global document:true */
+
+document.addEventListener('DOMContentLoaded', function () {
 
 	var divForName = document.querySelector('.button-name');
-	divForName.addEventListener('click', createNewName);
 
-	function createNewName(){
-		var newName = new Name('bcdfghjklmnpqrstvwxz', 'aeiouy', 'name', randomNumber(5,4));
-		newName
-			.setFirstLetter()
-			.generateName()
-			.firstLetterUpper();
-		console.log(newName);
-	}
-
-	function Name(_consonant, _vowel, _localization, _nameLength){
+	function Name(_consonant, _vowel, _localization, _nameLength) {
 		this.consonant = _consonant;
 		this.vowel = _vowel;
 		this.consonantLength = this.consonant.length;
@@ -22,24 +14,28 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		this.where = document.querySelector('.name');
 	}
 
-	Name.prototype.setFirstLetter = function(){
-		this.whatLetterFirst = randomNumber(2,0);
-		if(this.whatLetterFirst == 1){
+	function randomNumber(to, from) {
+		return Math.floor(Math.random() * to) + from;
+	}
+
+	Name.prototype.setFirstLetter = function () {
+		this.whatLetterFirst = randomNumber(2, 0);
+		if (this.whatLetterFirst === 1) {
 			this.name += this.consonant.charAt(randomNumber(this.consonantLength, 0));
 			this.nextLetter = 'vowel';
-		}else if(this.whatLetterFirst === 0){
+		} else if (this.whatLetterFirst === 0) {
 			this.name += this.vowel.charAt(randomNumber(this.vowelLength, 0));
 			this.nextLetter = 'consonant';
 		}
 		return this;
 	};
 
-	Name.prototype.generateName = function(){
-		for( var i = 0; i < this.nameLength; i += 1 ){
-			if(this.nextLetter === 'consonant'){
+	Name.prototype.generateName = function () {
+		for (var i = 0; i < this.nameLength; i += 1) {
+			if (this.nextLetter === 'consonant') {
 				this.name += this.consonant.charAt(randomNumber(this.consonant.length, 0));
 				this.nextLetter = 'vowel';
-			}else{
+			} else {
 				this.name += this.vowel.charAt(randomNumber(this.vowel.length, 0));
 				this.nextLetter = 'consonant';
 			}
@@ -47,13 +43,20 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		return this;
 	};
 
-	Name.prototype.firstLetterUpper = function(){
-		return this.where.innerHTML = this.name.charAt(0).toUpperCase() + this.name.slice(1); 
+	Name.prototype.firstLetterUpper = function () {
+		this.where.innerHTML = this.name.charAt(0).toUpperCase() + this.name.slice(1);
+		return this;
 	};
 
-	function randomNumber(to, from){
-		return Math.floor(Math.random() * to) + from;
+	function createNewName() {
+		var newName = new Name('bcdfghjklmnpqrstvwxz', 'aeiouy', 'name', randomNumber(5, 4));
+		newName
+			.setFirstLetter()
+			.generateName()
+			.firstLetterUpper();
 	}
+
+	divForName.addEventListener('click', createNewName);
 
 });
 
