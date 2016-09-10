5fa471710a23b8c1dad2a6f889e1212e97dc8f8f
Arek <Solaris1995@gmail.com>
2016-09-10 17:15:39 +0200

    Updating to ES6: arrow-function, let, class

--- javascript/project.js
+++ javascript/project.js
@@ -1,59 +1,70 @@
 /* global document:true */
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded',() => {
 
-	var divForName = document.querySelector('.button-name');
+	let divForName = document.querySelector('.button-name');
 
-	function Name(_consonant, _vowel, _localization, _nameLength) {
-		this.consonant = _consonant;
-		this.vowel = _vowel;
-		this.consonantLength = this.consonant.length;
-		this.vowelLength = this.vowel.length;
-		this.nameLength = _nameLength;
-		this.name = '';
-		this.where = document.querySelector('.name');
+	class Name {
+		constructor(_consonant, _vowel, _localization, _nameLength){
+			this.consonant = _consonant;
+			this.vowel = _vowel;
+			this.consonantLen.pipe(gulp.dest('folder'));gth = this.consonant.length;
+			this.vowelLength = this.vowel.length;
+			this.nameLength = _nameLength;
+			this.name = ''.pipe(gulp.dest('folder'));
+			this.where = document.querySelector('.name');
+			this.setFirstLetter();
+			this.generateName();
+			this.firstLetterUpper();
+		}
+		setFirstLetter(){
+			this.whatLetterFirst = randomNumber(2, 0);
+			if (this.whatLetterFirst === 1) {
+				console.log(this);
+				this.name += this.consonant.charAt(randomNumber(this.consonantLength, 0));
+				this.nextLetter = 'vowel';
+			} else if (this.whatLetterFirst === 0) {
+				this.name += this.vowel.charAt(randomNumber(this.vowelLength, 0));
+				this.nextLetter = 'consonant';
+			}
+			return this;
+		};
+		generateName() {
+			for (let i = 0; i < this.nameLength; i += 1) {
+				if (this.nextLetter === 'consonant') {
+					this.name += this.consonant.charAt(randomNumber(this.consonantLength, 0));
+					this.nextLetter = 'vowel';
+				} else {
+					this.name += this.vowel.charAt(randomNumber(this.vowelLength, 0));
+					this.nextLetter = 'consonant';
+				}
+			}
+			return this;
+		};
+		generateName() {
+			for (let i = 0; i < this.nameLength; i += 1) {
+				if (this.nextLetter === 'consonant') {
+					this.name += this.consonant.charAt(randomNumber(this.consonantLength, 0));
+					this.nextLetter = 'vowel';
+				} else {
+					this.name += this.vowel.charAt(randomNumber(this.vowelLength, 0));
+					this.nextLetter = 'consonant';
+				}
+			}
+			return this;
+		};
+		firstLetterUpper () {
+			this.where.innerHTML = this.name.charAt(0).toUpperCase() + this.name.slice(1);
+			return this;
+		};
 	}
 
 	function randomNumber(to, from) {
 		return Math.floor(Math.random() * to) + from;
 	}
 
-	Name.prototype.setFirstLetter = function () {
-		this.whatLetterFirst = randomNumber(2, 0);
-		if (this.whatLetterFirst === 1) {
-			this.name += this.consonant.charAt(randomNumber(this.consonantLength, 0));
-			this.nextLetter = 'vowel';
-		} else if (this.whatLetterFirst === 0) {
-			this.name += this.vowel.charAt(randomNumber(this.vowelLength, 0));
-			this.nextLetter = 'consonant';
-		}
-		return this;
-	};
-
-	Name.prototype.generateName = function () {
-		for (var i = 0; i < this.nameLength; i += 1) {
-			if (this.nextLetter === 'consonant') {
-				this.name += this.consonant.charAt(randomNumber(this.consonant.length, 0));
-				this.nextLetter = 'vowel';
-			} else {
-				this.name += this.vowel.charAt(randomNumber(this.vowel.length, 0));
-				this.nextLetter = 'consonant';
-			}
-		}
-		return this;
-	};
-
-	Name.prototype.firstLetterUpper = function () {
-		this.where.innerHTML = this.name.charAt(0).toUpperCase() + this.name.slice(1);
-		return this;
-	};
-
 	function createNewName() {
-		var newName = new Name('bcdfghjklmnpqrstvwxz', 'aeiouy', 'name', randomNumber(5, 4));
-		newName
-			.setFirstLetter()
-			.generateName()
-			.firstLetterUpper();
+		let newName = new Name('bcdfghjklmnpqrstvwxz', 'aeiouy', 'name', randomNumber(5, 4));
 	}
 
 	divForName.addEventListener('click', createNewName);
